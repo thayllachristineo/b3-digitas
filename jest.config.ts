e334a172cdd2a66d431c72bc840a57d53cbdb9ee
thaylla <like.baby3450@gmail.com>
2023-07-28 10:12:26 -0300

    feat: install and configure test environment

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  moduleDirectories: ['node_modules', 'src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  preset: 'ts-jest/presets/default-esm',
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+    '\\.svg$': '<rootDir>/src/__mocks__/svgrMock.ts',
+  },
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
+
+export default defineConfig((props) => {
+  const env = loadEnv(props.mode, process.cwd(), 'VITE');
+  const envWithProcessPrefix = {
+    'process.env': `${JSON.stringify(env)}`,
+  };
+
+  return {
+    plugins: [react(), svgr()],
+    define: envWithProcessPrefix,
+  };
 });
