051bfa4599805258c81355287f3df85f4b29b48f
thaylla <like.baby3450@gmail.com>
2023-07-28 10:14:12 -0300

    feat: create Table.utils suite test

--- src/components/Table/Table.utils.ts
+++ src/components/Table/Table.utils.ts
@@ -2,6 +2,6 @@ const locale = 'pt-BR';
 
 const transformRateToLocale = (rate: number) => rate.toLocaleString(locale, { style: 'currency', currency: 'BRL' });
 
-const transformDateToLocale = (date: string) => new Date(date).toLocaleDateString(locale);
+const transformDateToLocale = (date: string) => new Date(date).toLocaleDateString(locale, { timeZone: 'UTC' });
 
 export { transformRateToLocale, transformDateToLocale };
